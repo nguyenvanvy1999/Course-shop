083f78eee16283fb366d54f2fdb4694accab7a85
Nguyen Van Vy <nguyenvanvy1999@gmail.com>
2021-02-11 08:48:06 +0700

    config project

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+    morgan = require('morgan'),
+    handlebars = require('express-handlebars'),
+    path = require('path'),
+    app = express(),
+    port = 8080;
+
+app.use(morgan('dev'));
+
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(port, () =>
+    console.log(`App listening at http://localhost:${port}`)
+);
